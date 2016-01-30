1fe33f8561114df819dd05fa907b44bd15a1d97c
Brian Stone <brianpatrickstone@gmail.com>
2016-01-29 21:41:35 -0700

    Provided an 'Add another file' link that uses JavaScript so that users can upload more than one file

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :attachments, only: [:show]
+  resources :attachments, only: [:show, :new]
 
   devise_for :users
     root "projects#index"
